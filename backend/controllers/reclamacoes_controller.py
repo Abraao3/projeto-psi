4767e8ca6f1dd0b71d01220f0f59b8879147010e
Pedro Victor <azevedodvictor@gmail.com>
2025-12-02 15:26:04 -0300

    feat: listagem de reclamações com filtros por status implementada na api

--- backend/controllers/reclamacoes_controller.py
+++ backend/controllers/reclamacoes_controller.py
@@ -1,143 +1,33 @@
-from flask import Blueprint, jsonify, request
-from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from backend.models import Usuario
-from backend.extensions import db
+from flask import Blueprint, jsonify
+from backend.models import StatusReclamacao, Reclamacao
 
 
 reclamacoes_bp = Blueprint('reclamacoes', __name__, url_prefix='/reclamacoes')
 
 @reclamacoes_bp.route('/')
 def reclamacoes():
-    lista_reclamacoes = { 
-        "reclamacoes": [
-            {
-                "id": 1,
-                "autor": "Pedro Victor",
-                "titulo": "buraco",
-                "descricao": "buraco gigantesco na rua de divanilson",
-                "cidade": "Caicó-RN",
-                "status": "pendente"
-            },
-            {
-                "id": 2,
-                "autor": "Abraão",
-                "titulo": "cachorro comendo pneu de motoqueiro",
-                "descricao": "tem muito cachorro comendo pneu aqui",
-                "cidade": "Caicó-RN",
-                "status": "resolvida"
-            },
-            {
-                "id": 3,
-                "autor": "Givanilson",
-                "titulo": "tão assando gato",
-                "descricao": "abraao abriu um churrasco de gato no serrote branco",
-                "cidade": "Caicó-RN",
-                "status": "contestada"
-            }
-        ]
-    }
+    reclamacoes: list[Reclamacao] = Reclamacao.query.all()
+    reclamacoes_to_dict = [reclamacao.to_dict() for reclamacao in reclamacoes]
 
+    return jsonify({"reclamacoes": reclamacoes_to_dict}), 200
 
-    return jsonify(lista_reclamacoes), 200
+@reclamacoes_bp.route('/pendentes')
+def reclamacoes_pendentes():
+    reclamacoes: list[Reclamacao] = Reclamacao.query.filter(Reclamacao.status == StatusReclamacao.PENDENTE).all()
+    reclamacoes_to_dict = [reclamacao.to_dict() for reclamacao in reclamacoes]
+
+    return jsonify({"reclamacoes": reclamacoes_to_dict}), 200
 
 @reclamacoes_bp.route('/resolvidas')
 def reclamacoes_resolvidas():
-    lista_reclamacoes = { 
-        "reclamacoes": [
-            {
-                "id": 1,
-                "autor": "Pedro Victor",
-                "titulo": "buraco",
-                "descricao": "buraco gigantesco na rua de divanilson",
-                "cidade": "Caicó-RN",
-                "status": "resolvida"
-            },
-            {
-                "id": 2,
-                "autor": "Abraão",
-                "titulo": "cachorro comendo pneu de motoqueiro",
-                "descricao": "tem muito cachorro comendo pneu aqui",
-                "cidade": "Caicó-RN",
-                "status": "resolvida"
-            },
-            {
-                "id": 3,
-                "autor": "Givanilson",
-                "titulo": "tão assando gato",
-                "descricao": "abraao abriu um churrasco de gato no serrote branco",
-                "cidade": "Caicó-RN",
-                "status": "resolvida"
-            }
-        ]
-    }
+    reclamacoes: list[Reclamacao] = Reclamacao.query.filter(Reclamacao.status == StatusReclamacao.RESOLVIDA).all()
+    reclamacoes_to_dict = [reclamacao.to_dict() for reclamacao in reclamacoes]
 
-
-    return jsonify(lista_reclamacoes), 200
+    return jsonify({"reclamacoes": reclamacoes_to_dict}), 200
 
 @reclamacoes_bp.route('/contestadas')
 def reclamacoes_contestadas():
-    lista_reclamacoes = { 
-        "reclamacoes": [
-            {
-                "id": 1,
-                "autor": "Pedro Victor",
-                "titulo": "buraco",
-                "descricao": "buraco gigantesco na rua de divanilson",
-                "cidade": "Caicó-RN",
-                "status": "contestada"
-            },
-            {
-                "id": 2,
-                "autor": "Abraão",
-                "titulo": "cachorro comendo pneu de motoqueiro",
-                "descricao": "tem muito cachorro comendo pneu aqui",
-                "cidade": "Caicó-RN",
-                "status": "contestada"
-            },
-            {
-                "id": 3,
-                "autor": "Givanilson",
-                "titulo": "tão assando gato",
-                "descricao": "abraao abriu um churrasco de gato no serrote branco",
-                "cidade": "Caicó-RN",
-                "status": "contestada"
-            }
-        ]
-    }
+    reclamacoes: list[Reclamacao] = Reclamacao.query.filter(Reclamacao.status == StatusReclamacao.CONTESTADA).all()
+    reclamacoes_to_dict = [reclamacao.to_dict() for reclamacao in reclamacoes]
 
-
-    return jsonify(lista_reclamacoes), 200
-@reclamacoes_bp.route('/pendentes')
-def reclamacoes_pendentes():
-    lista_reclamacoes = { 
-        "reclamacoes": [
-            {
-                "id": 1,
-                "autor": "Pedro Victor",
-                "titulo": "buraco",
-                "descricao": "buraco gigantesco na rua de divanilson",
-                "cidade": "Caicó-RN",
-                "status": "pendente"
-            },
-            {
-                "id": 2,
-                "autor": "Abraão",
-                "titulo": "cachorro comendo pneu de motoqueiro",
-                "descricao": "tem muito cachorro comendo pneu aqui",
-                "cidade": "Caicó-RN",
-                "status": "pendente"
-            },
-            {
-                "id": 3,
-                "autor": "Givanilson",
-                "titulo": "tão assando gato",
-                "descricao": "abraao abriu um churrasco de gato no serrote branco",
-                "cidade": "Caicó-RN",
-                "status": "pendente"
-            }
-        ]
-    }
-
-
-    return jsonify(lista_reclamacoes), 200
+    return jsonify({"reclamacoes": reclamacoes_to_dict}), 200
